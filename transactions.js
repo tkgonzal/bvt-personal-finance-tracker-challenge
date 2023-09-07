744630acd0b8442b78fa97c35760eda4466179d5
Traven Gonzales <travengonzales@gmail.com>
2023-09-07 16:44:03 -0700

    refactor(transaction-item-generation): Refactored item generation to be in its own function and use constants

--- transactions.js
+++ transactions.js
@@ -1,5 +1,33 @@
 const fs = require("fs");
 
+const ITEM_KEYS = ["name", "category", "amount"];
+
+// Item Object Generation Function
+/**
+ * @param {string[]} itemArgs An array of arguments passed to the script
+ * specifying key value pairs for an item to insert into the ledger
+ * @returns {Object} An item object to insert into the ledger
+ */
+const makeNewItem = itemArgs => {
+    const newItem = {};
+    
+    itemArgs.forEach(arg => {
+        const equalsIndex = arg.indexOf("=");
+        const key = arg.slice(1, equalsIndex)
+        const val = arg.slice(equalsIndex + 1);
+
+        if (!key | !ITEM_KEYS.includes(key)) {
+            throw new TypeError(`Argument key must be -name, -category, or -amount, not ${key ? `${key}`: "empty"}`);
+        } 
+
+        newItem[key] = key !== "amount" ? val : parseFloat(val);
+    });
+
+    newItem["timestamp"] = new Date();
+
+    return newItem;
+}
+
 // --------MAIN DRIVER--------
 (() => {
     try {
@@ -9,21 +37,7 @@ const fs = require("fs");
             throw new TypeError("Must provide arguments for exactly -name, -category, and -amount");
         }
     
-        const newItem = {};
-    
-        args.forEach(arg => {
-            const equalsIndex = arg.indexOf("=");
-            const key = arg.slice(1, equalsIndex)
-            const val = arg.slice(equalsIndex + 1);
-
-            if (!key | !["name", "category", "amount"].includes(key)) {
-                throw new TypeError(`Argument key must be -name, -category, or -amount, not ${key ? `${key}`: "empty"}`);
-            } 
-
-            newItem[key] = key !== "amount" ? val : parseFloat(val);
-        });
-
-        newItem["timestamp"] = new Date();
+        const newItem = makeNewItem(args);
 
         console.log(newItem);
     } catch (error) {
